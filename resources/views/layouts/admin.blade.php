a64cd15d26683dd5bfc0d94b91e1686af0bc588b
Đỗ Hồng Quân <quandohong28@192.168.0.102>
2024-09-20 12:18:53 +0700

    refactor: Update admin layout structure

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,26 +7,24 @@
     <link rel="shortcut icon" href="{{ asset('logo-light.svg') }}" type="image/x-icon">
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
 </head>
 
 <body class="bg-gray-50 dark:bg-gray-800 text-sm">
     @include('partials.admin.header')
 
-    <div class="flex pt-16 overflow-hidden bg-gray-50 dark:bg-gray-900">
-        @include('partials.admin.sidebar')
+    <div class="flex pt-16">
+        <aside>
+            @include('partials.admin.sidebar')
+        </aside>
 
-        <div class="fixed inset-0 z-10 hidden bg-gray-900/50 dark:bg-gray-900/90" id="sidebarBackdrop"></div>
-
-        <div class="min-h-screen relative bg-gray-50 lg:ms-64 dark:bg-gray-900">
+        <main class="min-h-screen w-full lg:ml-64 p-4 bg-gray-50 dark:bg-gray-900">
             @yield('content')
-        </div>
-
+        </main>
     </div>
 
-    <div class="overflow-y-auto bg-gray-50 lg:ml-64 dark:bg-gray-900">
+    <footer class="bg-gray-50 dark:bg-gray-900 lg:ml-64">
         @include('partials.admin.footer')
-    </div>
+    </footer>
 
     <script src="{{ asset('dist/flowbite.min.js') }}"></script>
 </body>
